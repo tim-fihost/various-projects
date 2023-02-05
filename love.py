363a7d22d017fbdb9a7cc66051574c9e67070e2a
tim-fihost <92898560+tim-fihost@users.noreply.github.com>
2023-02-05 21:19:45 +0900

    Add files via upload

---
+++ love.py
@@ -0,0 +1,29 @@
+#overflow 
+#love calculator
+name1 = "Killer Bee" #input("Your name: ")
+name2 = "Lady Tsunade" #input("Her or His name: ")
+name1 = name1.lower()
+name2 = name2.lower()
+combined_string = name1 + name2
+logic = "true love"
+#True Love
+a = 0
+b = 0
+for i in combined_string:
+    a = a + "true".count(i)
+    b = b + "love".count(i)
+score = int(str(a)+str(b))
+if score < 40:
+    print("You guys will be like coke and mentos!:0")
+    print(f"Your score is {score}%")
+elif score < 60:
+    print("Well you guys are okay, but maybe you shoud rethink before to date?")
+    print(f"Your score is {score}%")
+elif score < 80:
+    print("Congrats you guys will be a great couple!")
+    print(f"Your score is {score}%")
+elif score < 100:
+    print("Wow you guys are more than couples!!")
+    print(f"Your score is {score}%")
+else:
+    print("Ups something went wrond re-run program again!")
