e6e281af2e82771981e76c738301bde92e61ea15
Timur-Malik <92898560+tim-fihost@users.noreply.github.com>
2023-10-27 08:42:59 -0400

    Add files via upload

---
+++ wiki.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+LINK = "https://en.wikipedia.org/wiki/Main_Page"
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("datch",True)
+
+driver = webdriver.Chrome()
+driver.get(LINK)
+
+#articles = driver.find_element(By.XPATH, value='//*[@id="articlecount"]/a[1]') 
+articles = driver.find_element(By.CSS_SELECTOR, "#articlecount a")
+print(articles.text)
